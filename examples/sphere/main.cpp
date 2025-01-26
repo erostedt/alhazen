9612e85e7429a2d820c07c2800912f8e7b0ccc8b
Eric Rostedt <rostedteric@gmail.com>
2025-01-26 18:40:20 +0100

    change props

--- examples/sphere/main.cpp
+++ examples/sphere/main.cpp
@@ -8,9 +8,13 @@
 int main()
 {
     const Scene scene = {{Sphere{{0, 0, -1}, 0.5f}, Sphere{{0, -100.5, -1}, 100.0f}}};
-    const Camera camera = CreateCamera({0, 0, 0}, {0, 0, -1}, CameraProperties{});
+
+    CameraProperties props;
+    props.ImageWidth = 400;
+
+    const Camera camera = CreateCamera({0, 0, 0}, {0, 0, -1}, props);
     const u32 rays_per_pixel = 10;
-    const u32 max_bounces = 10;
+    const u32 max_bounces = 50;
     FloatImage image = RenderImage(camera, scene, rays_per_pixel, max_bounces);
     WriteFloatImageToPPM(std::cout, image);
 }
