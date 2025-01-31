b46a3fa8279a2b65d6914d3048dca9528c7b8f28
Eric Rostedt <rostedteric@gmail.com>
2025-01-31 10:40:14 +0100

    inline and noexcept

--- alhazen/vec3.cpp
+++ alhazen/vec3.cpp
@@ -4,91 +4,7 @@
 
 #include "random.hpp"
 
-Vec3 operator+(Vec3 u, Vec3 v)
-{
-    return {u.X + v.X, u.Y + v.Y, u.Z + v.Z};
-}
-
-Vec3 operator+(Vec3 v, f32 s)
-{
-    return {v.X + s, v.Y + s, v.Z + s};
-}
-
-Vec3 operator+(f32 s, Vec3 v)
-{
-    return v + s;
-}
-
-Vec3 operator-(Vec3 u, Vec3 v)
-{
-    return {u.X - v.X, u.Y - v.Y, u.Z - v.Z};
-}
-
-Vec3 operator-(Vec3 v, f32 s)
-{
-    return {v.X - s, v.Y - s, v.Z - s};
-}
-
-Vec3 operator-(f32 s, Vec3 v)
-{
-    return {s - v.X, s - v.Y, s - v.Z};
-}
-
-Vec3 operator-(Vec3 v)
-{
-    return {-v.X, -v.Y, -v.Z};
-}
-
-Vec3 operator*(Vec3 v, f32 s)
-{
-    return {v.X * s, v.Y * s, v.Z * s};
-}
-
-Vec3 operator*(f32 s, Vec3 v)
-{
-    return v * s;
-}
-
-Vec3 operator/(Vec3 v, f32 s)
-{
-    return {v.X / s, v.Y / s, v.Z / s};
-}
-
-f32 Dot(Vec3 u, Vec3 v)
-{
-    return u.X * v.X + u.Y * v.Y + u.Z * v.Z;
-}
-
-f32 SquaredLength(Vec3 v)
-{
-    return Dot(v, v);
-}
-
-f32 Length(Vec3 v)
-{
-    return std::sqrt(SquaredLength(v));
-}
-
-Vec3 Normalized(Vec3 v)
-{
-    return v / Length(v);
-}
-
-Vec3 Cross(Vec3 u, Vec3 v)
-{
-    Vec3 c;
-    c.X = u.Y * v.Z - u.Z * v.Y;
-    c.Y = u.Z * v.X - u.X * v.Z;
-    c.Z = u.X * v.Y - u.Y * v.X;
-    return c;
-}
-
-bool NearZero(Vec3 v, f32 tolerance)
-{
-    return SquaredLength(v) < tolerance;
-}
-
-Vec3 RandomUnitVector()
+Vec3 RandomUnitVector() noexcept
 {
     Vec3 v;
     while (true)
@@ -103,7 +19,7 @@ Vec3 RandomUnitVector()
     }
 }
 
-Vec3 RandomVectorOnHemisphere(Vec3 normal)
+Vec3 RandomVectorOnHemisphere(Vec3 normal) noexcept
 {
     Vec3 on_unit_sphere = RandomUnitVector();
     if (Dot(on_unit_sphere, normal) > 0.0f)
@@ -112,7 +28,7 @@ Vec3 RandomVectorOnHemisphere(Vec3 normal)
         return -on_unit_sphere;
 }
 
-Vec3 RandomVectorOnUnitDisk()
+Vec3 RandomVectorOnUnitDisk() noexcept
 {
     Vec3 v;
     v.Z = 0.0f;
--- alhazen/vec3.hpp
+++ alhazen/vec3.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #include "types.hpp"
 
 struct Vec3
@@ -9,32 +11,93 @@ struct Vec3
     f32 Z;
 };
 
-Vec3 operator+(Vec3 u, Vec3 v);
-Vec3 operator+(Vec3 v, f32 s);
-Vec3 operator+(f32 s, Vec3 v);
+inline Vec3 operator+(Vec3 u, Vec3 v) noexcept
+{
+    return {u.X + v.X, u.Y + v.Y, u.Z + v.Z};
+}
 
-Vec3 operator-(Vec3 u, Vec3 v);
-Vec3 operator-(Vec3 v, f32 s);
-Vec3 operator-(f32 s, Vec3 v);
+inline Vec3 operator+(Vec3 v, f32 s) noexcept
+{
+    return {v.X + s, v.Y + s, v.Z + s};
+}
 
-Vec3 operator-(Vec3 v);
+inline Vec3 operator+(f32 s, Vec3 v) noexcept
+{
+    return v + s;
+}
 
-Vec3 operator*(Vec3 v, f32 s);
-Vec3 operator*(f32 s, Vec3 v);
+inline Vec3 operator-(Vec3 u, Vec3 v) noexcept
+{
+    return {u.X - v.X, u.Y - v.Y, u.Z - v.Z};
+}
 
-Vec3 operator/(Vec3 v, f32 s);
+inline Vec3 operator-(Vec3 v, f32 s) noexcept
+{
+    return {v.X - s, v.Y - s, v.Z - s};
+}
 
-f32 Dot(Vec3 u, Vec3 v);
-f32 SquaredLength(Vec3 v);
-f32 Length(Vec3 v);
-Vec3 Normalized(Vec3 v);
-Vec3 Cross(Vec3 u, Vec3 v);
+inline Vec3 operator-(f32 s, Vec3 v) noexcept
+{
+    return {s - v.X, s - v.Y, s - v.Z};
+}
 
-bool NearZero(Vec3 v, f32 tolerance = 1e-8f);
+inline Vec3 operator-(Vec3 v) noexcept
+{
+    return {-v.X, -v.Y, -v.Z};
+}
 
-Vec3 RandomUnitVector();
-Vec3 RandomVectorOnHemisphere(Vec3 normal);
-Vec3 RandomVectorOnUnitDisk();
+inline Vec3 operator*(Vec3 v, f32 s) noexcept
+{
+    return {v.X * s, v.Y * s, v.Z * s};
+}
+
+inline Vec3 operator*(f32 s, Vec3 v) noexcept
+{
+    return v * s;
+}
+
+inline Vec3 operator/(Vec3 v, f32 s) noexcept
+{
+    return {v.X / s, v.Y / s, v.Z / s};
+}
+
+inline f32 Dot(Vec3 u, Vec3 v) noexcept
+{
+    return u.X * v.X + u.Y * v.Y + u.Z * v.Z;
+}
+
+inline f32 SquaredLength(Vec3 v) noexcept
+{
+    return Dot(v, v);
+}
+
+inline f32 Length(Vec3 v) noexcept
+{
+    return std::sqrt(SquaredLength(v));
+}
+
+inline Vec3 Normalized(Vec3 v) noexcept
+{
+    return v / Length(v);
+}
+
+inline Vec3 Cross(Vec3 u, Vec3 v) noexcept
+{
+    Vec3 c;
+    c.X = u.Y * v.Z - u.Z * v.Y;
+    c.Y = u.Z * v.X - u.X * v.Z;
+    c.Z = u.X * v.Y - u.Y * v.X;
+    return c;
+}
+
+inline bool NearZero(Vec3 v, f32 tolerance = 1e-8f) noexcept
+{
+    return SquaredLength(v) < tolerance;
+}
+
+Vec3 RandomUnitVector() noexcept;
+Vec3 RandomVectorOnHemisphere(Vec3 normal) noexcept;
+Vec3 RandomVectorOnUnitDisk() noexcept;
 
 const Vec3 ZERO = {0.0f, 0.0f, 0.0f};
 const Vec3 RIGHT = {1.0f, 0.0f, 0.0f};
