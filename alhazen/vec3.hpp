73bec802cb4c26ed62356039c430a92bba6285f0
Eric Rostedt <rostedteric@gmail.com>
2025-01-31 09:09:07 +0100

    add zero vector

--- alhazen/vec3.hpp
+++ alhazen/vec3.hpp
@@ -36,6 +36,7 @@ Vec3 RandomUnitVector();
 Vec3 RandomVectorOnHemisphere(Vec3 normal);
 Vec3 RandomVectorOnUnitDisk();
 
+const Vec3 ZERO = {0.0f, 0.0f, 0.0f};
 const Vec3 RIGHT = {1.0f, 0.0f, 0.0f};
 const Vec3 UP = {0.0f, 1.0f, 0.0f};
 const Vec3 FORWARD = {0.0f, 0.0f, -1.0f};
