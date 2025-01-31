2ca75503b78f702e76c4ad10189cf62a23609579
Eric Rostedt <rostedteric@gmail.com>
2025-01-31 10:58:14 +0100

    more inlining

--- alhazen/ray.hpp
+++ alhazen/ray.hpp
@@ -8,7 +8,14 @@ struct Ray
 {
     Point3 Origin;
     Vec3 Direction;
-    Point3 At(f32 t) const;
+
+    inline Point3 At(f32 t) const noexcept
+    {
+        return Origin + t * Direction;
+    }
 };
 
-bool FrontFacing(const Ray &r, Vec3 outward_normal);
+inline bool FrontFacing(const Ray &r, Vec3 outward_normal)
+{
+    return Dot(r.Direction, outward_normal) < 0.0f;
+}
