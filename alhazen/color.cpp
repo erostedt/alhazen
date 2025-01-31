6ec40b9c60b48656759ccefd5c2a01c69f9d4b06
Eric Rostedt <rostedteric@gmail.com>
2025-01-31 09:06:30 +0100

    use operator overloading

--- alhazen/color.cpp
+++ alhazen/color.cpp
@@ -39,11 +39,7 @@ Color operator/(Color c, f32 s)
 
 Color LinearBlend(Color c1, Color c2, f32 a)
 {
-    Color blended;
-    blended.Red = a * c1.Red + (1.0f - a) * c2.Red;
-    blended.Green = a * c1.Green + (1.0f - a) * c2.Green;
-    blended.Blue = a * c1.Blue + (1.0f - a) * c2.Blue;
-    return blended;
+    return a * c1 + (1.0f - a) * c2;
 }
 
 Color LinearToGamma(Color c)
