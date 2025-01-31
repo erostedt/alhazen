0af66d9ed5ee11052cd5144d7c5ded13a9426444
Eric Rostedt <rostedteric@gmail.com>
2025-01-31 19:42:20 +0100

    add additional functions for interval

--- alhazen/interval.hpp
+++ alhazen/interval.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "types.hpp"
+#include <cmath>
 
 struct Interval
 {
@@ -17,3 +18,21 @@ struct Interval
         return (LowerBound < x) && (x < UpperBound);
     }
 };
+
+inline Interval CreateInterval(f32 lower_bound, f32 upper_bound)
+{
+    Interval interval;
+    interval.LowerBound = std::fmin(lower_bound, upper_bound);
+    interval.UpperBound = std::fmax(lower_bound, upper_bound);
+    return interval;
+}
+
+inline Interval Range(Interval i1, Interval i2) noexcept
+{
+    return {std::fmin(i1.LowerBound, i2.LowerBound), std::fmax(i1.UpperBound, i2.UpperBound)};
+}
+
+inline Interval Overlap(Interval i1, Interval i2)
+{
+    return {std::fmax(i1.LowerBound, i2.LowerBound), std::fmin(i1.UpperBound, i2.UpperBound)};
+}
