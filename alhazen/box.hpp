c4fd905717ab65ee26059839d2a7e3e122f7cdea
Eric Rostedt <rostedteric@gmail.com>
2025-02-03 17:15:56 +0100

    add check box ray intersection

--- alhazen/box.hpp
+++ alhazen/box.hpp
@@ -2,6 +2,7 @@
 
 #include "interval.hpp"
 #include "point3.hpp"
+#include "ray.hpp"
 
 struct Box
 {
@@ -43,11 +44,38 @@ inline Box CreateBox(Point3 p, Point3 q)
     return box;
 }
 
-inline Box Expand(Box b1, Box b2)
+inline Box Expand(const Box &b1, const Box &b2)
 {
     Box expanded;
     expanded.XAxis = Expand(b1.XAxis, b2.XAxis);
     expanded.YAxis = Expand(b1.YAxis, b2.YAxis);
     expanded.ZAxis = Expand(b1.ZAxis, b2.ZAxis);
     return expanded;
+}
+
+inline bool HitsBox(const Box &box, const Ray &ray, Interval interval) noexcept
+{
+    f32 hit1 = (box.XAxis.LowerBound - ray.Origin.X) / ray.Direction.X;
+    f32 hit2 = (box.XAxis.UpperBound - ray.Origin.X) / ray.Direction.X;
+    Interval axis = CreateInterval(hit1, hit2);
+    interval = Overlap(axis, interval);
+    if (interval.IsEmpty())
+    {
+        return false;
+    }
+
+    hit1 = (box.YAxis.LowerBound - ray.Origin.Y) / ray.Direction.Y;
+    hit2 = (box.YAxis.UpperBound - ray.Origin.Y) / ray.Direction.Y;
+    axis = CreateInterval(hit1, hit2);
+    interval = Overlap(axis, interval);
+    if (interval.IsEmpty())
+    {
+        return false;
+    }
+
+    hit1 = (box.ZAxis.LowerBound - ray.Origin.Z) / ray.Direction.Z;
+    hit2 = (box.ZAxis.UpperBound - ray.Origin.Z) / ray.Direction.Z;
+    axis = CreateInterval(hit1, hit2);
+    interval = Overlap(axis, interval);
+    return !interval.IsEmpty();
 }
