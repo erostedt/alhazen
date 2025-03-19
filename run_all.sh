1ba2ae6d8c5061c3cd14428746be7a2272383d7b
Eric Rostedt <rostedteric@gmail.com>
2025-03-19 09:09:12 +0100

    add missing examples

--- run_all.sh
+++ run_all.sh
@@ -3,9 +3,12 @@
 mkdir -p outputs
 
 time ./build/bouncing_spheres >outputs/bouncing_spheres.ppm
+time ./build/box >outputs/box.ppm
 time ./build/checkered_spheres >outputs/checkered_spheres.ppm
+time ./build/cornell_box >outputs/cornell_box.ppm
 time ./build/earth >outputs/earth.ppm
 time ./build/perlin_spheres >outputs/perlin_spheres.ppm
 time ./build/quads >outputs/quads.ppm
 time ./build/raytracing_in_a_weekend >outputs/raytracing_in_a_weekend.ppm
+time ./build/raytracing_the_next_week >outputs/raytracing_the_next_week.ppm
 time ./build/simple_light >outputs/simple_light.ppm
